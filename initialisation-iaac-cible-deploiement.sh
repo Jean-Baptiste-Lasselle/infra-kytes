8aebf3c0ba06856ea6d08a6c85d4eef5b391a32b
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-09-16 15:26:00 -0400

    Update initialisation-iaac-cible-deploiement.sh

--- initialisation-iaac-cible-deploiement.sh
+++ initialisation-iaac-cible-deploiement.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+cat ./.env|grep 
+VERSION_IMAGE_CENTOS=centos:7
+VERSION_IMAGE_MONGO=mongo:latest
+VERSION_IMAGE_GITLAB_CE=gitlab/gitlab-ce:latest
+VERSION_IMAGE_GITLAB_RUNNER=gitlab/gitlab-runner:latest
+VERSION_IMAGE_ROCKETCHAT=rocketchat/rocket.chat:latest
+VERSION_IMAGE_HUBOT_ROCKETCHAT=rocketchat/hubot-rocketchat:latest
+VERSION_IMAGE_RNGINX=nginx:latest
+
 # + Permet d'initialiser le contexte de déploimeent, la cible de déploiement, pour un cycle IAAC
 docker pull centos:7
 docker pull mongo:latest
