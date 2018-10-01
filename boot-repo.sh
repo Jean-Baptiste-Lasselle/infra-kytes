a9e5fdd68ac73d3b2751e8b1c8006a40d85c4aaf
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-01 06:13:25 -0400

    Update boot-repo.sh

--- boot-repo.sh
+++ boot-repo.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # Ce script permet d'initialiser ce repository Git, à partir d'un ensemble de fichiers, ajoutés au premier commit.
+
+# - ENV.
+
+# - OPS
+git init
+git add --all
+git add **/*
+git commit -m "Commit initial de la Recette Provision de l'innfrastructure Kytes"
+git remote add origin git@github.com:Jean-Baptiste-Lasselle/infra-kytes.git
+git push -u origin master
