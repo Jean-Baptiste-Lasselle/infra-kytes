c1cba348cb5a3bf3dcd9ad811aef6dd688fc1f69
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-11-05 00:20:36 -0500

    Update initialisation-iaac-cible-deploiement.sh

--- initialisation-iaac-cible-deploiement.sh
+++ initialisation-iaac-cible-deploiement.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# -- Le tout doit correspondre aux variables déclarées en fichier de fichier ./.env : 
+
+# # -- VERSIONS IMAGES DOCKER -- 
+# # + À noter : ces variables d'envrionnement sont utilisée à la fois dans le docker-compose.yml, mais ne peuvent être utilisées dans les fichiers Dockerfile ('on recourera à du template jinja2 / ansible)
+# VERSION_IMAGE_CENTOS=centos:7
+# # VERSION_IMAGE_GITLAB_CE=gitlab/gitlab-ce:$GITLAB_CE_VERSION
+# # VERSION_IMAGE_GITLAB_RUNNER=gitlab/gitlab-runner:${GITLAB_RUNNER_VERSION}
+# VERSION_IMAGE_NGINX=nginx:latest
+# VERSION_IMAGE_ROCKETCHAT=rocketchat/rocket.chat:latest
+# VERSION_IMAGE_MONGO=mongo:latest
+# # VERSION_IMAGE_POSTGRES=postgres:${POSTGRES_VERSION}
+# # VERSION_IMAGE_REDIS=redis:{REDIS_VERSION}
+
+# -- 
+
 
 # VERSION_IMAGE_CENTOS=centos:7
 export ID_IMAGE_CENTOS=$(cat ./.env|grep VERSION_IMAGE_CENTOS | awk -F = '{print $2}')
@@ -17,6 +32,16 @@ export ID_IMAGE_GITLAB_CE="gitlab/gitlab-ce:$CETTE_GITLAB_CE_VERSION"
 # export ID_IMAGE_GITLAB_RUNNER=$(cat ./.env|grep VERSION_IMAGE_GITLAB_RUNNER | awk -F = '{print $2}')
 export ID_IMAGE_GITLAB_RUNNER=$(cat ./.env|grep GITLAB_RUNNER_VERSION | awk -F = '{print $2}')
 export ID_IMAGE_GITLAB_RUNNER="gitlab/gitlab-runner:$ID_IMAGE_GITLAB_RUNNER"
+
+# VERSION_IMAGE_GITLAB_CE=gitlab/gitlab-ce:latest
+# export ID_IMAGE_GITLAB_CE=$(cat ./.env|grep VERSION_IMAGE_GITLAB_CE | awk -F = '{print $2}')
+export CETTE_POSTGRES_VERSION=$(cat ./.env|grep POSTGRES_VERSION | awk -F = '{print $2}')
+export ID_IMAGE_POSTGRES="postgres:$POSTGRES_VERSION"
+
+# VERSION_IMAGE_GITLAB_CE=gitlab/gitlab-ce:latest
+# export ID_IMAGE_GITLAB_CE=$(cat ./.env|grep VERSION_IMAGE_GITLAB_CE | awk -F = '{print $2}')
+export CETTE_REDIS_VERSION=$(cat ./.env|grep REDIS_VERSION | awk -F = '{print $2}')
+export ID_IMAGE_REDIS="redis:REDIS_VERSION"
 
 # VERSION_IMAGE_ROCKETCHAT=rocketchat/rocket.chat:latest
 export ID_IMAGE_ROCKETCHAT=$(cat ./.env|grep VERSION_IMAGE_ROCKETCHAT | awk -F = '{print $2}')
@@ -39,6 +64,10 @@ echo "    ID_IMAGE_GITLAB_CE=$ID_IMAGE_GITLAB_CE "
 echo "  -------------------------------------------  "
 echo "    ID_IMAGE_GITLAB_RUNNER=$ID_IMAGE_GITLAB_RUNNER "
 echo "  -------------------------------------------  "
+echo "    ID_IMAGE_POSTGRES=$ID_IMAGE_POSTGRES "
+echo "  -------------------------------------------  "
+echo "    ID_IMAGE_REDIS=$ID_IMAGE_REDIS "
+echo "  -------------------------------------------  "
 echo "    ID_IMAGE_ROCKETCHAT=$ID_IMAGE_ROCKETCHAT "
 echo "  -------------------------------------------  "
 echo "    ID_IMAGE_NGINX=$ID_IMAGE_NGINX "
@@ -48,10 +77,12 @@ echo "   "
 
 
 docker system prune -f
-# + Permet d'initialiser le contexte de déploimeent, la cible de déploiement, pour un cycle IAAC
+# + Permet d'initialiser le contexte de déploiement, la cible de déploiement, pour un cycle IAAC
 docker pull "$ID_IMAGE_CENTOS"
 docker pull "$ID_IMAGE_MONGO"
 docker pull "$ID_IMAGE_GITLAB_CE"
 docker pull "$ID_IMAGE_GITLAB_RUNNER"
+docker pull "$ID_IMAGE_POSTGRES"
+docker pull "$ID_IMAGE_REDIS"
 docker pull "$ID_IMAGE_ROCKETCHAT"
 docker pull "$ID_IMAGE_NGINX"
